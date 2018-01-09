748d9fd21c45eff2a9f053fe64173c3d698dae3a
DeeLMind <DeeLMind@gmail.com>
2018-01-09 14:56:48 +0800

    Add-1

--- ATHydra.py
+++ ATHydra.py
@@ -1,1 +1,46 @@
 #ATHydra
+
+class ATHydra:
+    def __banner(self):
+        print("""
+        ATHydra:
+                To Easy To USE Hydra
+                By SwordHeart Team
+                Github  :https://github.com/DeeLMind
+                Blog    :http://www.cnblogs.com/DeeLMind
+        """)
+
+    def __init__(self):
+        self.__banner()
+        pass
+
+    def __del__(self):
+        pass
+
+
+    #private list of user
+    __User = []
+    __Pass = []
+
+    def __load(self,path,Lst):
+        with open(path,"r") as rFile:
+            for line in rFile.readlines():
+                Lst.append(line)
+
+    def loadUser(self,path,lUser):
+        self.__load(path,lUser)
+
+    def loadPass(self,path,lPass):
+        self.__load(path,lPass)
+
+
+A = ATHydra()
+
+
+
+
+
+
+
+
+
